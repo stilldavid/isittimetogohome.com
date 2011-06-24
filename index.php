dca629ec27dc25d39ee1190272f94b7a05a4c7cf
Dave Stillman <dave@sparkfun.com>
2011-06-24 15:37:20 -0600

    bored friday cleanups

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 
 function get_t_zone($hour) {
   $diff = abs(17 - $hour);
-  
+
   if (17 < $hour)
     $diff = -$diff;
-  
+
   if ($diff > 12)
     $diff -= 24;
 
@@ -42,32 +42,42 @@ $to_print = get_city($diff);
 $text = isset($_GET['t']) ? htmlspecialchars($_GET['t']) : "Yes.";
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <!--
-                 _                          _ 
+                 _                          _
                 | |                        | |
     __ _  ___   | |__   ___  _ __ ___   ___| |
    / _` |/ _ \  | '_ \ / _ \| '_ ` _ \ / _ \ |
   | (_| | (_) | | | | | (_) | | | | | |  __/_|
    \__, |\___/  |_| |_|\___/|_| |_| |_|\___(_)
-    __/ |                                     
+    __/ |
    |___/
-  
+
   But seriously, stop reading the source and GTFO
   of work. It's five o'clock somewhere...
-  
+
   My name is David Stillman, by the way. I like
   the Internet.
-  
+
   http://stilldavid.com
-  
+
 -->
-  <head> 
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
-    <title>is it time to go home?</title> 
-    <style type="text/css"> 
+
+    <script type="text/javascript">
+      var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+      document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var pageTracker = _gat._getTracker("UA-8612424-1");
+        pageTracker._trackPageview();
+      } catch(err) {}
+    </script>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>is it time to go home?</title>
+    <style type="text/css">
       body {
         text-align: center;
         font-family: helvetica, sans-serif;
@@ -83,22 +93,12 @@ $text = isset($_GET['t']) ? htmlspecialchars($_GET['t']) : "Yes.";
         margin: 1em;
         color: #AAA;
       }
-    </style> 
-  </head> 
-  <body> 
-    <p class="big"><?= $text ?></p> 
+    </style>
+  </head>
+  <body>
+    <p class="big"><?= $text ?></p>
     <p class="small">At least it is in <?= $to_print ?></p>
-
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-8612424-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-</body>
+  </body>
   <?
    /*
    for($i=0; $i<24; $i++) {
